256feedf7d51b4c908d5d620ba474c6f1c3b9cb4
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-06-24 11:46:18 +0530

    send http request with thunk action creator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "./components/Store/notificationSlice";
-import Notification from './components/UI/Notification';
-
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./components/Store/CartSlice";
 
 let isInitial = true;
 
@@ -15,65 +15,27 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://api-call-51521-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cartItemsData),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cartItemsData));
   }, [cartItemsData, dispatch]);
 
   return (
     <>
-    {notification && (
-      <Notification
-        status={notification.status}
-        title={notification.title}
-        message={notification.message}
-      />
-    )}
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
-  </>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        <Cart />
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/Store/CartSlice.js
+++ src/components/Store/CartSlice.js
@@ -1,3 +1,5 @@
+import { uiActions } from "./notificationSlice";
+
 const { createSlice } = require("@reduxjs/toolkit");
 
 const cartIntialState = { isShow: false, cartItems: [] };
@@ -33,6 +35,47 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cartItemsData) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    try {
+      const response = await fetch(
+        "https://api-call-51521-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartItemsData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      } else {
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Sent cart data successfully!",
+          })
+        );
+      }
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const { removeItemFromCart } = cartSlice.actions;
 export const addItemToCartAction = cartSlice.actions.addItemToCart;
